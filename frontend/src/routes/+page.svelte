e74204f0496a13d66f515bc5ae786a2fa8dbaee8
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-08-04 15:33:20 -0700

    fetch data onmount

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,15 +5,12 @@
 
     let books = writable([]);
 
+    // Fetch data from the server
     onMount(async () => {
-        await fetchBooks();
-    });
-
-    async function fetchBooks() {
         const response = await fetch('http://localhost:8000/book');
         const data = await response.json();
         books.set(data);
-    }
+    });
 </script>
 
 <ListBooks {books} />
